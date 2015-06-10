f60b5ffe8bda597b035041127e613b2028a20b94
rofrischmann <robin@rofrischmann.de>
2015-06-10 23:34:11 +0200

    refactored Stylesheet
    removed media queries and hover by now, will add support later with
    different methods

--- example/LayoutTest.react.js
+++ example/LayoutTest.react.js
@@ -1,35 +1,38 @@
 var React = require('react');
-var Stylesheet = require('../src/Stylesheet.js').Stylesheet;
+var Stylesheet = require('../src/Stylesheet.js');
 
 var LayoutTest = React.createClass({
-
-    render: function () {
-        var styles = Stylesheet.create(stylesheet);
-        return (
-            <div style={styles.box}>Blaaa
-               <div style={styles.item}>Hallo</div>
+   render: function () {
+      var styleString = JSON.stringify(styles);
+      return (
+         <div>
+            <div style={styles.box}>{styleString}
             </div>
-        );
-    }
+            <div style={styles.item}>TestElement</div>
+         </div>
+      );
+   }
 });
-var stylesheet = {
-    box: {
-        backgroundColor: "blue",
-        padding: 20,
-        borderColor: "green",
-        borderWidth: 1,
-        borderStyle: "solid",
-        transition: '200ms all linear'
-    },
-    item: {
-        borderColor: "green",
-        borderWidth: 1,
-        borderStyle: "solid",
-        backgroundColor: "green",
-        padding: 30,
-        color: "white",
-        borderRadius: 10
-    }
-};
+
+var styles = Stylesheet.create({
+   box: {
+      backgroundColor: "grey",
+      wordWrap: 'break-word',
+      padding: 20,
+      borderColor: "blue",
+      borderWidth: 1,
+      borderStyle: "solid",
+      transition: '200ms all linear'
+   },
+   item: {
+      borderColor: "green",
+      borderWidth: 1,
+      borderStyle: "solid",
+      backgroundColor: "blue",
+      padding: 30,
+      color: "white",
+      borderRadius: 10
+   }
+});
 
 module.exports = LayoutTest;
