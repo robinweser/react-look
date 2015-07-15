b797b7c0f9e2081df08aa99cacb49d86939c6a78
rofrischmann <robin@rofrischmann.de>
2015-07-16 00:07:19 +0200

    added 'root' as State API's default key, this closes #23

--- src/map/state.js
+++ src/map/state.js
@@ -1,5 +1,5 @@
 import assign from 'object-assign';
-
+const defaultKey = 'root';
 /*
  * A small helper module to handle States within react-look
  */
@@ -7,35 +7,42 @@ export default {
 	/*
 	 * Set/Add a new key with some new object
 	 */
-	add(container, key) {
+	add(container, key=defaultKey) {
 			return container.state._look.set(key, new Map());
 		},
 
 		/*
 		 * Get all states/information about a key
 		 */
-		get(container, key) {
+		get(container, key=defaultKey) {
 			return container.state._look.get(key);
+		},
+		
+		/*
+		 * Sets a whole new state map for a key
+		 */
+		set(states, container, key=defaultKey) {
+			return container.state._look.set(key, states);
 		},
 
 		/*
 		 * Check if there's already information about a key
 		 */
-		has(container, key) {
+		has(container, key=defaultKey) {
 			return container.state._look.has(key);
 		},
 
 		/*
 		 * Get a specific state of a key
 		 */
-		getState(state, container, key) {
+		getState(state, container, key=defaultKey) {
 			return container.state._look.get(key).get(state);
 		},
 
 		/*
 		 * Set a key's state
 		 */
-		setState(state, value, container, key) {
+		setState(state, value, container, key=defaultKey) {
 			container.state._look.get(key).set(state, value);
 			return container.setState(container.state._look);
 		},
@@ -43,7 +50,7 @@ export default {
 		/*
 		 * Check if a key has a specific state
 		 */
-		hasState(state, container, key) {
+		hasState(state, container, key=defaultKey) {
 			return container.state._look.has(key).has(state);
 		}
 }
