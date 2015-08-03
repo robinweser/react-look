3a4c4b97b909426422142124ab1cba648dee7f9f
rofrischmann <robin@rofrischmann.de>
2015-08-03 12:01:55 +0200

    #35 added a real cloneObject function to improve performance
    this closes #35

--- src/utils/cloner.js
+++ src/utils/cloner.js
@@ -1,8 +1,15 @@
 /**
  * Clones a Object
  * @param {Object} object - Object that get's cloned
- * TODO: optimize this step
+ * Taken from: http://heyjavascript.com/4-creative-ways-to-clone-objects/
  */
 export default function cloneObject(obj) {
-	return JSON.parse(JSON.stringify(obj));
+	if (obj === null || typeof obj !== 'object') {
+		return obj;
+	}
+	var temp = obj.constructor();
+	for (var key in obj) {
+		temp[key] = cloneObject(obj[key]);
+	}
+	return temp;
 }
