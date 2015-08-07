d48b3e5fe1fcf934b35d0914daeab6b845a358a8
rofrischmann <robin@rofrischmann.de>
2015-08-07 12:17:29 +0200

    #31 added other evaluation tests

---
+++ test/evaluation/pseudo/other.js
@@ -0,0 +1,20 @@
+import evaluatePseudoClass from '../../../lib/utils/evaluator/pseudo';
+import {expect} from 'chai';
+
+describe('Evaluating other pseudo classes', () => {
+	it(':lang(en) should return true if lang=en', () => {
+			expect(evaluatePseudoClass(':lang(en)', {lang: 'en'}, new Map(), {})).to.equal(true);
+	});
+	it(':lang(en) should return false if lang=de', () => {
+			expect(evaluatePseudoClass(':lang(en)', {lang: 'de'}, new Map(), {})).to.equal(false);
+	});
+	it(':empty should return true if element got no children', () => {
+			expect(evaluatePseudoClass(':empty', {children: []}, new Map(), {})).to.equal(true);
+	});
+	it(':empty should return true if element got no children', () => {
+			expect(evaluatePseudoClass(':empty', {}, new Map(), {})).to.equal(true);
+	});
+	it(':empty should return false if element got children', () => {
+			expect(evaluatePseudoClass(':empty', {children: ['test']}, new Map(), {})).to.equal(false);
+	});
+});
