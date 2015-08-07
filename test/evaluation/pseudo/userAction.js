8877fde854dd03d6e1bcb1adf76f5c9a2e89c4ad
rofrischmann <robin@rofrischmann.de>
2015-08-07 12:17:19 +0200

    #31 added user-action tests

---
+++ test/evaluation/pseudo/userAction.js
@@ -0,0 +1,41 @@
+import evaluatePseudoClass from '../../../lib/utils/evaluator/pseudo';
+import {expect} from 'chai';
+
+describe('Evaluating user-action pseudo classes', () => {
+	var keyStates = {
+		':hover': new Map([
+			['hover', true]
+		]),
+		':focus': new Map([
+			['focus', true]
+		]),
+		':active': new Map([
+			['active', true]
+		])
+	}
+
+	var keyStatesFalse = {
+		':hover': new Map([
+			['hover', false]
+		]),
+		':focus': new Map([
+			['focus', false]
+		]),
+		':active': new Map([
+			['active', false]
+		])
+	}
+
+	var state;
+	for (state in keyStates){
+		it(state + ' should return true', () => {
+			expect(evaluatePseudoClass(state, {}, keyStates[state], {})).to.equal(true);
+		});
+		it(state + ' should return false', () => {
+			expect(evaluatePseudoClass(state, {}, keyStatesFalse[state], {})).to.equal(false);
+		});
+		it(state + ' should return false with empty keyState', () => {
+			expect(evaluatePseudoClass(state, {}, new Map(), {})).to.equal(false);
+		});
+	}
+});
