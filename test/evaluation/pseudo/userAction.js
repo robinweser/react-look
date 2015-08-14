3566d03dbbebe03478252dd26311be1b32aac7a6
rofrischmann <robin@rofrischmann.de>
2015-08-14 11:26:58 +0200

    #52 cleaned other tests

--- test/evaluation/pseudo/userAction.js
+++ test/evaluation/pseudo/userAction.js
@@ -28,12 +28,17 @@ describe('Evaluating user-action pseudo classes', () => {
 
 	var state;
 	for (state in keyStates){
+		
 		it(state + ' should return true', () => {
 			expect(evaluatePseudoClass(state, {}, keyStates[state], {})).to.equal(true);
 		});
+		
+		
 		it(state + ' should return false', () => {
 			expect(evaluatePseudoClass(state, {}, keyStatesFalse[state], {})).to.equal(false);
 		});
+		
+		
 		it(state + ' should return false with empty keyState', () => {
 			expect(evaluatePseudoClass(state, {}, new Map(), {})).to.equal(false);
 		});
