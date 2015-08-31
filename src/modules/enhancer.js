706f1f368037b10bce39222c04a465b8b638d00b
rofrischmann <robin@rofrischmann.de>
2015-08-31 11:18:30 +0200

    cleaned enhancer

--- src/modules/enhancer.js
+++ src/modules/enhancer.js
@@ -1,5 +1,5 @@
 import resolveLook from './resolver';
-import {_Object, _Validator} from 'type-utils';
+import assign from 'object-assign';
 import assignStyles from 'assign-styles';
 import extend from '../utils/extend';
 
@@ -15,38 +15,9 @@ export default function Look(Component, additionalStyles = {}, additionalProcess
 			super(...arguments);
 			this.state = this.state ||  {};
 
-			//resolve processors
-			if (this.processors) {
-				if (this.processors instanceof Function) {
-					this.processors = this.processors();
-				}
-				//arrayify processors
-				if (this.processors instanceof Array !== true) {
-					this.processors = [this.processors];
-				}
-			}
 
-			//add additional processors
-			if (additionalProcessors && !this.processors) {
-				this.processors = [];
-			};
-
-			if (additionalProcessors instanceof Array) {
-				this.processors.push(...additionalProcessors);
-			} else if (additionalProcessors instanceof Object) {
-				this.processors.push(additionalProcessors);
-			}
-
-			//resolve mixins			
-			if (this.mixins) {
-				if (this.mixins instanceof Function) {
-					this.mixins = this.mixins();
-				}
-			} else {
-				this.mixins = {};
-			}
-			//Adds default extend-mixin support
-			this.mixins.extend = extend;
+			this.processors = prepareProcessors(this, additionalProcessors);
+			this.mixins = prepareMixins(this);
 
 			this._lastActive = [];
 			this.state._look = new Map();
@@ -65,36 +36,13 @@ export default function Look(Component, additionalStyles = {}, additionalProcess
 
 		//Similar to Radium, Look wraps the render function and resolves styles on its own
 		render() {
-			//resolve multiple styles by merging those
-			if (additionalStyles instanceof Array) {
-				this.styles = assignStyles(...additionalStyles);
-			} else if (additionalStyles instanceof Object) {
-				this.styles = additionalStyles;
-			} else {
-				console.warn('Additional styles need to be either a valid object or an array of valid objects.');
-				console.warn('Look ignores the following additional styles: ', additionalStyles);
-				this.styles = {};
-			}
-
-			//Merge component assigned styles with outer styles to 
-			if (this.look && this.look instanceof Function) {
-				this.styles = assignStyles(this.look(), this.styles);
-				delete this.look;
-			}
-
-			//Resolve default style object if no outer selector is given
-			if (this.styles[Object.keys(this.styles)[0]] instanceof Object !== true) {
-				this.styles = {
-					'_default': this.styles
-				}
-			}
-
+			this.styles = prepareStyles(this, additionalStyles);
 			/**
 			 * Only resolveLook if there are styles to resolve
 			 * Otherwise just return super.render() which leads to no difference
 			 */
-			if (this.styles || !_Validator.isEmpty(this.styles)) {
-				this._matchValues = _Object.assign({}, this.props, this.state);
+			if (this.styles && Object.keys(this.styles).length > 0) {
+				this._matchValues = assign({}, this.props, this.state);
 				return resolveLook(this, super.render());
 			} else {
 				console.warn(Compoent + ' was enhanced with Look, but did not provide any styles.');
@@ -108,4 +56,94 @@ export default function Look(Component, additionalStyles = {}, additionalProcess
 	LookComponent.displayName = Component.displayName || Component.name || 'Component';
 
 	return LookComponent;
+}
+
+/**
+ * Prepares processors and adds them to one unified array
+ * @param {Object} Component - Component providing processors
+ * @param {Object|Array} additionalProcessors - any additional processors provided by outer wrapper
+ */
+export function prepareProcessors(Component, additionalProcessors) {
+	let newProcessors;
+	
+	if (Component.processors) {
+		if (Component.processors instanceof Function) {
+			Component.processors = Component.processors();
+			newProcessors = Component.processors;
+		}
+		//arrayify processors
+		if (Component.processors instanceof Array !== true) {
+			newProcessors = [Component.processors];
+		}
+	}
+
+	//add additional processors
+	if (additionalProcessors) {
+		if (!Component.processors) {
+			newProcessors = [];
+		}
+		if (additionalProcessors instanceof Array) {
+			newProcessors.push(...additionalProcessors);
+		} else if (additionalProcessors instanceof Object) {
+			newProcessors.push(additionalProcessors);
+		}
+	}
+	
+	return newProcessors;
+}
+
+/**
+* Prepares mixins and adds extend mixin support
+* @param {Object} Component - Component providing mixins
+*/
+export function prepareMixins(Component){	
+	let newMixins;
+	
+	if (Component.mixins) {
+		if (Component.mixins instanceof Function) {
+			newMixins = Component.mixins();
+		} else if (Component.mixins instanceof Object){
+			newMixins = Component.mixins;
+		}
+	} else {
+		newMixins = {};
+	}
+	
+	//Adds default extend-mixin support
+	newMixins.extend = extend;
+	return newMixins;
+}
+
+/**
+ * Prepares Component styles and additionalStyles for later use as a single style object
+ * @param {Object} Component - Component providing styles
+ * @param {Object|Array} additionalStyles - any additional styles provided by outer wrapper
+ */
+export function prepareStyles(Component, additionalStyles){
+	let newStyles;
+	
+	if (additionalStyles instanceof Array) {
+	 	newStyles= assignStyles(...additionalStyles);
+	} else if (additionalStyles instanceof Object) {
+		newStyles = additionalStyles;
+	} else {
+		console.warn('Additional styles need to be either a valid object or an array of valid objects.');
+		console.warn('Look ignores the following additional styles: ', additionalStyles);
+		newStyles = {};
+	}
+
+	//Merge component assigned styles with outer styles to 
+	if (Component.look && Component.look instanceof Function) {
+		newStyles = assignStyles(Component.look(), newStyles);
+		delete Component.look;
+	}
+
+	//Resolve default style object if no outer selector is given
+	if (newStyles[Object.keys(newStyles)[0]] instanceof Object !== true) {
+		newStyles = {
+			'_default': newStyles
+		}
+	}
+	
+	return newStyles;
 }
