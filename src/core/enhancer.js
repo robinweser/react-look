24a9c1567e5e46853ae1e662b6426fd4e28e56e6
rofrischmann <robin@rofrischmann.de>
2015-09-02 11:18:39 +0200

    #66 added Enhancer to core with some small improvements

---
+++ src/core/enhancer.js
@@ -0,0 +1,80 @@
+import assign from 'object-assign'
+import assignStyles from 'assign-styles'
+import resolveLook from './resolver'
+
+export default function Look(Component, additionalStyles) {
+	class EnhancedComponent extends Component {
+		constructor() {
+			super(...arguments)
+			this.state = this.state ||  {}
+
+			this._lastActiveElements = []
+			this.state._look = new Map()
+		}
+
+		render() {
+			this.styles = prepareStyles(this, additionalstyles)
+
+			// Only resolve if there are styles to resolve
+			// Otherwise just return super.render() which leads to no difference
+			if (this.styles && Object.keys(this.styles).length > 0) {
+				this._matchValues = assign({}, this.props, this.state)
+				return resolveLook(this, super.render())
+			} else {
+				console.warn(Component + ' was enhanced with Look, but did not provide any styles.')
+				console.warn('This might affect performance and rendering time.')
+				return super.render()
+			}
+		}
+	}
+
+	//Inherit the original displayName for proper use later on
+	EnhancedComponent.displayName = Component.displayName || Component.name || 'Component'
+	return EnhancedComponent
+}
+
+
+/**
+ * Resolves any styles input into a valid single style object
+ * @param {Array|Object} styles - input to flatten
+ */
+export function flattenStyles(styles) {
+	if (styles instanceof Array) {
+		return assignStyles(...styles);
+	} else if (styles instanceof Function) {
+		return flattenStyles(styles())
+	} else if (styles instanceof Object) {
+		return styles
+	} else {
+		console.warn('Pass either a valid object or an array of valid objects.')
+		console.warn('Look can not flatten and will ignore the following styles input: ', styles)
+		return {}
+	}
+}
+
+
+
+
+/**
+ * Prepares Component styles and additional styles for later use as a single style object
+ * @param {Object} Component - Component providing styles
+ * @param {Object|Array} additionalStyles - any additional styles provided by outer wrapper
+ */
+export function prepareStyles(Component, additionalStyles) {
+	let styles = flattenStyles(additionalStyles ? additionalStyles : {})
+
+	if (Component.look) {
+		styles = assignStyles(flattenStyles(Component.look), styles)
+		delete Component.look
+	}
+
+	//In order to provide as less boilerplate as possible Look provides
+	//a shortcut to use a default selector if only passing a single style object
+	if (styles[Object.keys(styles)[0]] instanceof Object !== true) {
+		styles = {
+			'_default': styles
+		}
+	}
+
+	return styles;
+}
---
+++ test/core/enhancer-test.js
@@ -0,0 +1,117 @@
+import {expect} from 'chai'
+import {Component} from 'react'
+import Look, {flattenStyles, prepareStyles} from '../../lib/core/enhancer'
+
+describe('Enhancing a Component', () => {
+	it('should use the same displayName', () => {
+		class Default extends Component {}
+		let Enhanced = Look(Default)
+		let instance = new Enhanced()
+
+		expect(instance.displayName).to.eql(Default.displayName);
+	})
+	
+	it('should set up initial state', () => {
+		class Default extends Component {}
+		let Enhanced = Look(Default);
+		let instance = new Enhanced();
+
+		expect(instance.state).to.have.property('_look');
+	})
+
+
+	it('should add an array which registers all clicked items', () => {
+		class Default extends Component {}
+		let Enhanced = Look(Default);
+		let instance = new Enhanced();
+
+		expect(instance._lastActiveElements).to.exist.and.to.be.instanceof(Array);
+	})
+	
+	
+	it('should merge existing state', () => {
+		class Default extends Component {
+			constructor() {
+				super(...arguments);
+				this.state = {
+					foo: 1
+				}
+			}
+		}
+
+		let Enhanced = Look(Default);
+		let instance = new Enhanced();
+
+		expect(instance.state).to.eql({
+			foo: 1,
+			_look: new Map()
+		})
+	})
+
+
+	it('should recieve props', () => {
+		class Default extends Component {
+			constructor() {
+				super(...arguments);
+			}
+		}
+
+		let Enhanced = Look(Default);
+		let instance = new Enhanced({
+			bar: 1
+		})
+
+		expect(instance.props).to.eql({
+			bar: 1
+		})
+	})
+})
+
+
+describe('Flattening styles', () => {
+	it('should return the input object', () => {
+		let input = {
+			color: 'red'
+		}
+		expect(flattenStyles(input)).to.eql(input);
+	})
+	
+	it('should merge an array of multiple objects', () => {
+		let base = {
+			color: 'red',
+			fontSize: 15
+		}
+		let extend = {
+			color: 'blue'
+		}
+		expect(flattenStyles([base, extend])).to.eql({color: 'blue', fontSize: 15})
+	})
+	
+	it('should warn and return an empty object', () => {
+		expect(flattenStyles(23)).to.eql({})
+	})
+	
+	it('should call functions and return the flattened output', () => {
+		function style(){
+			return {
+				color: 'red'
+			}
+		}
+		expect(flattenStyles(style)).to.eql({color: 'red'})
+	})
+})
+
+describe('Preparing look styles', () => {
+	it('should add a default selector', () => {
+		class Default extends Component {}
+		Default.look = {color: 'red'}
+		expect(prepareStyles(Default)).to.eql({'_default' : {color: 'red'}});
+	})
+	
+	it('should prefer additional styles while preparing', () => {
+		class Default extends Component {}
+		Default.look = {color: 'red', fontSize: 15}
+		let Enhanced = Look(Default, {color: 'blue'})
+		expect(prepareStyles(Enhanced)).to.eql({'_default' : {color: 'blue', fontSize: 15}});
+	})
+})
