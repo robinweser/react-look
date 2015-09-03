1ef4fd29b76d82cd1a774273310ec7923d0e8642
rofrischmann <robin@rofrischmann.de>
2015-09-03 13:13:29 +0200

    #66 added tests for core plugins and included in core package

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import Look from './core/enhancer'
 import State from './core/api/State'
 import Config from './core/api/Config'
 
+import Conditions from './core/plugins/condition'
+import Mixins from './core/plugins/mixin'
+Config.registerProcessor(Conditions)
+Config.registerProcessor(Mixins)
+
 export {
 	Look as default,
 	State,
---
+++ test/core/plugins/mixin-test.js
@@ -0,0 +1,57 @@
+import {expect} from 'chai'
+import Mixins from '../../../lib/core/processor/mixin'
+
+describe('Resolving mixins', () => {
+	it('should not modify anything', () => {
+		let styles = {
+			_default: {
+				color: 'blue'
+			}
+		}
+		expect(Mixins.process(styles, {})).to.eql({
+			_default: {
+				color: 'blue'
+			}
+		})
+	})
+	
+	
+	function mixin(obj){
+		return {color: obj.color + 'test'}
+	}
+	
+	let Component = {
+		mixins : {
+			mixin: mixin
+		}
+	}
+	
+	it('should add resolved mixins styles', () => {
+		let styles = {
+			_default: {
+				fontSize: 15,
+				mixin : {color:'blue'}
+			}
+		}
+		expect(Mixins.process(styles, Component)).to.eql({
+			_default: {
+				fontSize: 15,
+				color: 'bluetest'
+			}
+		})
+	})
+	
+	it('should overwrite resolved mixins styles', () => {
+		let styles = {
+			_default: {
+				color: 'red',
+				mixin : {color:'blue'}
+			}
+		}
+		expect(Mixins.process(styles, Component)).to.eql({
+			_default: {
+				color: 'bluetest'
+			}
+		})
+	})
+})
