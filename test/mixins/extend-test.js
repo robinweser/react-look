707ed7d681011a02b0a1d60b0c9e13e91bb56844
rofrischmann <robin@rofrischmann.de>
2015-09-10 16:49:51 +0200

    #69 added extending tests

---
+++ test/mixins/extend-test.js
@@ -0,0 +1,97 @@
+import {extend} from '../../lib/mixins/extend' 
+import {expect} from 'chai' 
+
+
+describe('Extending styles', () => {
+
+	it('should not do anything if no styles are passed', () => {
+		let extended = extend('extend', {}) 
+		expect(extended).to.eql({}) 
+	}) 
+
+
+	it('should return whole obj if no condition and no styles are passed', () => {
+		let extended = extend('extend', {
+			color: 'blue',
+			fontSize: 15
+		}) 
+		expect(extended).to.eql({
+			color: 'blue',
+			fontSize: 15
+		}) 
+	}) 
+
+
+	it('should return styles if no condition is passed', () => {
+		let extended = extend('extend', {
+			styles: {
+				color: 'blue',
+				fontSize: 15
+			}
+		}) 
+		expect(extended).to.eql({
+			color: 'blue',
+			fontSize: 15
+		}) 
+	}) 
+
+
+	it('should not styles if no condition is not fulfilled', () => {
+		let extended = extend('extend', {
+			styles: {
+				color: 'blue',
+				fontSize: 15
+			},
+			condition: (true == false)
+		}) 
+		expect(extended).to.equal(false) 
+	}) 
+
+
+	it('should not styles if no condition is not fulfilled', () => {
+		let extended = extend('extend', {
+			styles: {
+				color: 'blue',
+				fontSize: 15
+			},
+			condition: (true == true)
+		}) 
+		expect(extended).to.eql({
+			color: 'blue',
+			fontSize: 15
+		}) 
+	}) 
+
+
+	it('should merge styles if an array is passed', () => {
+		let extended = extend('extend', {
+			styles: [{
+				color: 'blue',
+				fontSize: 15
+			}, {
+				lineHeight: 10
+			}]
+		}) 
+		expect(extended).to.eql({
+			color: 'blue',
+			fontSize: 15,
+			lineHeight: 10
+		}) 
+	}) 
+
+
+	it('should overwrite existing values while merging styles', () => {
+		let extended = extend('extend', {
+			styles: [{
+				color: 'blue',
+				fontSize: 15
+			}, {
+				fontSize: 10
+			}]
+		}) 
+		expect(extended).to.eql({
+			color: 'blue',
+			fontSize: 10
+		}) 
+	}) 
+}) 
