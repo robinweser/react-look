76c553de32534dc44e73bc2c75a16cd3b9d74e87
rofrischmann <robin@rofrischmann.de>
2015-09-21 21:33:28 +0200

    #101 added throttling method to debounce eventListeners

--- src/mixins/media-query.js
+++ src/mixins/media-query.js
@@ -1,4 +1,5 @@
 import MixinTypes from '../utils/MixinTypes'
+import throttle from '../utils/throttle'
 
 /**
  * Evaluates if a media condition is fulfilled by using window.matchMedia
@@ -12,9 +13,9 @@ export default [{
 		let matchMedia = typeof window !== 'undefined' ? window.matchMedia : undefined
 		if (matchMedia !== undefined) {
 			if (!args.Component._mediaQueryListener) {
-				args.Component._mediaQueryListener = () => {
+				args.Component._mediaQueryListener = throttle(() => {
 					args.Component.forceUpdate()
-				}
+				}, 250)
 				window.addEventListener('resize', args.Component._mediaQueryListener)
 				
 				//Remove the listener if the component unmounts to keep things clean
