edfaded9b195ae02574cfd16b6705f94dd421ee6
rofrischmann <robin@rofrischmann.de>
2015-09-25 23:26:27 +0200

    fixed mediaQueryListener

--- src/mixins/media-query.js
+++ src/mixins/media-query.js
@@ -6,29 +6,29 @@ import throttle from '../utils/throttle'
  * NOTE: This won't work on server-side by default
  */
 export default [{
-	key: '@media',
-	type: MixinTypes.BEGINWITH,
-	fn: (key, styles, args) => {
-		//Check if browser supports window.matchMedia
-		let matchMedia = typeof window !== 'undefined' ? window.matchMedia : undefined
-		if (matchMedia !== undefined) {
-			if (!args.Component._mediaQueryListener) {
-				args.Component._mediaQueryListener = throttle(() => {
-					args.Component.forceUpdate()
-				}, 250)
-				window.addEventListener('resize', args.Component._mediaQueryListener)
-				
-				//Remove the listener if the component unmounts to keep things clean
-				let existingWillUnmount = args.Component.componentWillUnmount
-				args.Component.componentWillUnmount = () => {
-					existingWillUnmount && existingWillUnmount()
-					window.removeEventListener(args.Component._mediaQueryListener)
-				}
-			}
-			return matchMedia(key.replace('@media', '').trim()).matches ? styles : false
-		} else {
-			console.warn('Failed evaluating media query: ' + key + '. Your environment is not able to use window.matchMedia.');
-			return false
-		}
-	}
+  key: '@media',
+  type: MixinTypes.BEGINWITH,
+  fn: (key, styles, args) => {
+    //Check if browser supports window.matchMedia
+    let matchMedia = typeof window !== 'undefined' ? window.matchMedia : undefined
+    if (matchMedia !== undefined) {
+      if (!args.Component._mediaQueryListener) {
+        args.Component._mediaQueryListener = throttle(() => {
+          args.Component.forceUpdate()
+        }, 250)
+        window.addEventListener('resize', args.Component._mediaQueryListener)
+
+        //Remove the listener if the component unmounts to keep things clean
+        let existingWillUnmount = args.Component.componentWillUnmount
+        args.Component.componentWillUnmount = () => {
+          existingWillUnmount && existingWillUnmount()
+          window.removeEventListener('resize', args.Component._mediaQueryListener)
+        }
+      }
+      return matchMedia(key.replace('@media', '').trim()).matches ? styles : false
+    } else {
+      console.warn('Failed evaluating media query: ' + key + '. Your environment is not able to use window.matchMedia.');
+      return false
+    }
+  }
 }]
