0178b0b210afea7761f11f2b051902b3e2814728
David Ehlen <dehlen@me.com>
2015-10-06 21:58:34 +0200

    #91 we only the need expression here

--- src/utils/splitNthExpression.js
+++ src/utils/splitNthExpression.js
@@ -3,10 +3,7 @@
  * @param {string} pseudo - pseudo-class selector that includes a mathmactical expression
  * @param {string} expression - defines which index-sensitive pseudo-class your pseudo is, e.g: nth-child, first-of-type 
  */
-export default function splitNthExpression(pseudo) {
-	let split = pseudo.replace(/ /g, '').split('(');
-	return {
-		expression: split[1].substr(0, split[1].length - 1),
-		pseudo: split[0]
-	}
+export default function getNthExpression(pseudo) {
+  let split = pseudo.replace(/ /g, '').split('(');
+  return split[1].substr(0, split[1].length - 1)
 }
