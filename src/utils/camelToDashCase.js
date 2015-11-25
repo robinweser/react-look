f6bf42f01b4e72559e141e1590d6d016700d96ca
rofrischmann <robin@rofrischmann.de>
2015-11-25 23:05:10 +0100

    #142 added tests for camelToDashCase

--- src/utils/camelToDashCase.js
+++ src/utils/camelToDashCase.js
@@ -2,4 +2,4 @@
  * Converts a camel-case string to a dash-case string
  * @param {string} str - str that gets converted to dash-case
  */
-export default str => str.replace(/([a-z]|^)([A-Z])/g, (match, p1, p2) => p1 + '-' + p2.toLowerCase())
+export default str => str.replace(/([a-z]|^)([A-Z])/g, (match, p1, p2) => p1 + '-' + p2.toLowerCase()).replace('ms-', '-ms-')
