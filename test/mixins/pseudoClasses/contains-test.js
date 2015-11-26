be781872105914d95a7f83eef9b268269bfa485f
rofrischmann <robin@rofrischmann.de>
2015-11-26 15:36:34 +0100

    #142 added test for :contains

---
+++ test/mixins/pseudoClasses/contains-test.js
@@ -0,0 +1,17 @@
+import contains from '../../../lib/mixins/pseudoClasses/contains'
+import { expect } from 'chai'
+
+describe('Evaluating :contains pseudo', () => {
+
+  it('should validate true', () => {
+    const equal = {newProps: {children: 'foo'}}
+    const contain = {newProps: {children: 'foobar'}}
+    expect(contains(':contains(foo)', true, ':contains', equal)).to.equal(true)
+    expect(contains(':contains(ooba)', true, ':contains', contain)).to.equal(true)
+  })
+
+  it('should validate false', () => {
+    const args = {newProps: {children: 'foobar'}}
+    expect(contains(':contains(fooobar)', true, ':contains', args)).to.equal(false)
+  })
+})
