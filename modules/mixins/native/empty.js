748d13397834e4b47eaf8198b8894f2ac3f4a6d9
rofrischmann <robin@rofrischmann.de>
2016-02-16 16:56:59 +0100

    added :empty for react native

---
+++ modules/mixins/native/empty.js
@@ -0,0 +1,4 @@
+// Evaluates if an element is empty / got no children at all
+export default ({ property, value, mixinKey, newProps }) => {
+  return !newProps.children || (newProps.children && newProps.children.length < 1) ? value : false
+}
