26cd0a4cf868773926026c1e0533297acc9fae1c
Moaaz Sidat <moaaz.omer@gmail.com>
2016-03-20 18:20:18 -0400

    Added a test for friendlyClassNameTemplate, using a custom template instead of the default one

--- packages/react-look/test/plugins/friendlyClassName-test.js
+++ packages/react-look/test/plugins/friendlyClassName-test.js
@@ -29,4 +29,41 @@ describe('Transforming classNames to friendly class names', () => {
     friendlyClassName(pluginInterface)
     expect(newProps.className).to.eql('Comp-div--' + styles.box)
   })
+
+  it('should use a custom template if one is provided in the config', () => {
+    const styles = StyleSheet.create({
+      box: {
+        color: 'red',
+        '@media (min-height: 300px)': {
+          color: 'blue'
+        }
+      },
+      box2: {
+        color: 'orange',
+        '@media (min-height: 300px)': {
+          color: 'purple'
+        }
+      }
+    })
+    const Component = { constructor: { displayName: 'Comp' } }
+    const element = <div></div>
+    const newProps = { className: styles.box2 }
+    const config = {
+      friendlyClassNameTemplate: (cls, Component, element) => {
+       return Component.constructor.displayName + '-custom-' + 'div' + '--custom--' + cls
+      }
+    }
+    const pluginInterface = {
+      StyleContainer,
+      Component,
+      config,
+      element,
+      newProps,
+      styles: {}
+    }
+
+    friendlyClassName(pluginInterface)
+    expect(newProps.className).to.eql('Comp-custom-div--custom--' + styles.box2)
+
+  })
 })
